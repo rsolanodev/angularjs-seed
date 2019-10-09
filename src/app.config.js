c854ad9d81a02af5a6f1115a610f6c88f0691713
Ruben <rubensoljim@gmail.com>
2019-10-09 10:55:41 +0200

    CORS added

--- src/app.config.js
+++ src/app.config.js
@@ -1,3 +1,9 @@
+/* CORS */
+myApp.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.withCredentials = true;
+}])
+
+/* Delete Prefix from URL */
 myApp.config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('');
